2c4e1dca15ec09fa23451093249c56305e8f5fbb
cristiano <cristiano.souza.mendonca@gmail.com>
2014-08-03 12:19:17 -0300

    Finishing deploy.rb mina file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,15 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
-set :domain, '107.170.155.38'
+set :domain, 'societo.com.br'
 set :deploy_to, '/root/societo'
 set :repository, 'https://github.com/cristianocasm/societo.git'
 set :branch, 'ProducaoMode'
 set :term_mode, nil
 set :rvm_path, '/usr/local/rvm/scripts/rvm'
+set :rails_env, 'production'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
@@ -63,7 +57,7 @@ task :deploy => :environment do
 
     to :launch do
       queue "touch #{deploy_to}/tmp/restart.txt"
-      queue "thin start -e production --threaded"
+      queue "rvmsudo thin start --threaded -p 80"
     end
   end
 end
